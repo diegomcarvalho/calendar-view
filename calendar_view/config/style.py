53aae32924bb6a7930206f1bdec295fb98ce0bc9
Diego Carvalho <d.carvalho@ieee.org>
2024-03-02 07:53:51 -0300

    new lint

--- calendar_view/config/style.py
+++ calendar_view/config/style.py
@@ -1,11 +1,11 @@
 from PIL import ImageFont
 from pkg_resources import resource_filename
 
-font_path: str = 'Roboto-Regular.ttf'
+font_path: str = "Roboto-Regular.ttf"
 
 
 def image_font(size: int):
-    path: str = resource_filename('calendar_view.resources.fonts', font_path)
+    path: str = resource_filename("calendar_view.resources.fonts", font_path)
     return ImageFont.truetype(path, size)
 
 
@@ -17,17 +17,17 @@ padding_horizontal = 60
 padding_vertical = 30
 
 title_font = image_font(50)
-title_color = 'black'
+title_color = "black"
 title_padding_left = 30
 title_padding_right = 30
 title_padding_top = 30
 title_padding_bottom = 20
 
 hour_number_font = image_font(22)
-hour_number_color = 'black'
+hour_number_color = "black"
 
 day_of_week_font = image_font(28)
-day_of_week_color = 'black'
+day_of_week_color = "black"
 
 line_day_color = (150, 150, 150, 255)
 line_day_width = 5
@@ -40,9 +40,9 @@ event_border_default = (120, 180, 120, 240)
 event_fill_default = (196, 234, 188, 210)
 
 event_title_font = image_font(36)
-event_title_color = 'black'
+event_title_color = "black"
 event_notes_font = image_font(26)
-event_notes_color = 'gray'
+event_notes_color = "gray"
 event_padding: int = 20
 event_title_margin: int = 20
 
@@ -52,6 +52,6 @@ legend_padding_bottom = 70
 legend_padding_left = 70
 legend_padding_right = 40
 legend_name_font = image_font(28)
-legend_name_color = 'black'
+legend_name_color = "black"
 
 # https://stackoverflow.com/questions/7510313/transparent-png-in-pil-turns-out-not-to-be-transparent
